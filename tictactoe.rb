f08ae75ac26658ce053198569191a5bd79b9bab5
Rasmus Rajasalo <rarasmus@outlook.com>
2019-05-23 17:47:54 +0900

    Improve Game and Grid classes

--- tictactoe.rb
+++ tictactoe.rb
@@ -2,46 +2,38 @@
 class Grid
     attr_accessor :grid
 
-    BOX_ONE = 1
-    BOX_TWO = 2
-    BOX_THREE = 3
-    BOX_FOUR = 4
-    BOX_FIVE = 5
-    BOX_SIX = 6
-    BOX_SEVEN = 7
-    BOX_EIGHT = 8
-    BOX_NINE = 9
-
     def initialize
         @grid = "
                 |----|----|----|   
-                |  #{BOX_ONE} |  #{BOX_TWO} |  #{BOX_THREE} |
+                |  1 |  2 |  3 |
                 |----|----|----|
-                |  #{BOX_FOUR} |  #{BOX_FIVE} |  #{BOX_SIX} |
+                |  4 |  5 |  6 |
                 |----|----|----|
-                |  #{BOX_SEVEN} |  #{BOX_EIGHT} |  #{BOX_NINE} |
+                |  7 |  8 |  9 |
                 |----|----|----|
                 "
     end
 end
 
 class Game < Grid
-    @@player_one_turns = 0
-    @@win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     @@player_one = Array.new
     @@player_two = Array.new
+    @@player_one_turns = 0
+    @@win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    @@player_one_win = @@win.include?(@@player_one)
+    @@player_two_win = @@win.include?(@@player_two)
 
     def game()
         puts "**********"
         puts "This is a tic-tac-toe game for two human players."
         puts "You're the first player."
-            until @@player_one_turns == 5
+            until @@player_one_win || @@player_two_win
                 puts "Player One, make your choice."
                 puts @grid
                 player_one_choice()
                 puts
                 @@player_one_turns +=1
-                break if @@player_one_turns == 5
+                break if @@player_one.length == 5
                 puts "Player Two, make your choice."
                 puts @grid
                 player_two_choice()
@@ -50,14 +42,16 @@ class Game < Grid
     end
 
     def player_one_choice()
-        @@player_one << gets.chomp
+        @@player_one << gets.chomp.to_i
+        p @@player_one
         if @@win.include?(@@player_one)
             puts "The game has ended. Player One has won!"
         end
     end
 
     def player_two_choice()
-        @@player_two << gets.chomp
+        @@player_two << gets.chomp.to_i
+        p @@player_two
         if @@win.include?(@@player_two)
             puts "The game has ended. Player Two has won!"
         end
