ca1480404791dcf56cbf7821648b68937be663d0
Rasmus Rajasalo <rarasmus@outlook.com>
2019-05-23 17:03:08 +0900

    Edit some variables

--- tictactoe.rb
+++ tictactoe.rb
@@ -28,8 +28,8 @@ end
 class Game < Grid
     @@player_one_turns = 0
     @@win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-    @@player_one_moves = Array.new
-    @@player_two_moves = Array.new
+    @@player_one = Array.new
+    @@player_two = Array.new
 
     def game()
         puts "**********"
@@ -38,27 +38,27 @@ class Game < Grid
             until @@player_one_turns == 5
                 puts "Player One, make your choice."
                 puts @grid
-                player_one()
+                player_one_choice()
                 puts
                 @@player_one_turns +=1
                 break if @@player_one_turns == 5
                 puts "Player Two, make your choice."
                 puts @grid
-                player_two()
+                player_two_choice()
                 puts
             end
     end
 
-    def player_one()
-        @@player_one_moves << gets.chomp
-        if @@player_one_moves.include?(@@win)
+    def player_one_choice()
+        @@player_one << gets.chomp
+        if @@player_one.include?(@@win)
             puts "The game has ended. Player One has won!"
         end
     end
 
-    def player_two()
-        @@player_two_moves << gets.chomp
-        if @@player_two_moves.include?(@@win)
+    def player_two_choice()
+        @@player_two << gets.chomp
+        if @@player_two.include?(@@win)
             puts "The game has ended. Player Two has won!"
         end
     end
