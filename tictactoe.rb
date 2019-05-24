02bc8592cd90fa756dbfe84078b978739331c7d5
Rasmus Rajasalo <rarasmus@outlook.com>
2019-05-24 16:47:28 +0900

    Fix broken loop, game_end? and sort arrays

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,6 @@
 
 class Grid
+    WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
     attr_accessor :possible_choice
     attr_accessor :grid
 
@@ -20,44 +21,49 @@ end
 class Game < Grid
     @@player_one = Array.new
     @@player_two = Array.new
-    @@win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-    @@game_end = @@win.include?(@@player_one) || @@win.include?(@@player_two)
 
     def game
-        player_one_turns = 0
         puts "**********"
         puts "This is a tic-tac-toe game for two human players."
         puts "You're the first player."
-            until @@game_end == true
-                puts "Player One, make your choice."
-                p @possible_choice
-                puts @grid
-                player_one_choice()
-                puts
-                player_one_turns +=1
-                break if @@player_one.length == 5
-                puts "Player Two, make your choice."
-                p @possible_choice
-                puts @grid
-                player_two_choice()
-                puts
-            end
+        loop do
+            player_one_turn()
+            puts
+                if has_won?
+                    puts "The game has ended. Player One has won!"
+                    puts
+                    return
+                end
+            break if @@player_one.length == 5
+            player_two_turn()
+            puts
+                if has_won?
+                    puts "The game has ended. Player Two has won!"
+                    puts
+                    return
+                end
+        end
     end
 
-    def player_one_choice()
+    def player_one_turn()
+        puts "Player One, make your choice:"
+        p @possible_choice
+        puts @grid
         @@player_one << @possible_choice.delete(gets.chomp.to_i)
         p @@player_one
-        if @@win.include?(@@player_one)
-            puts "The game has ended. Player One has won!"
-        end
     end
 
-    def player_two_choice()
+    def player_two_turn()
+        puts "Player Two, make your choice:"
+        p @possible_choice
+        puts @grid
         @@player_two << @possible_choice.delete(gets.chomp.to_i)
         p @@player_two
-        if @@win.include?(@@player_two)
-            puts "The game has ended. Player Two has won!"
-        end
+    end
+
+    def has_won?
+        WINNING_COMBOS.include?(@@player_one.sort) ||
+        WINNING_COMBOS.include?(@@player_two.sort)
     end
 end
 
