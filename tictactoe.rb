07cdb0aa697d492d08746cd6011c7d9259c2cab0
Rasmus Rajasalo <rarasmus@outlook.com>
2019-05-28 16:06:24 +0900

    Improve has_won? and change Arrays to Sets

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,12 @@
 
 class Grid
-    WINNING_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    require 'set'
+
+    WINNING_COMBOS = [
+        [1,2,3],[4,5,6],[7,8,9],
+        [1,4,7],[2,5,8],[3,6,9],
+        [1,5,9],[3,5,7]
+    ].map(&:to_set)
     attr_accessor :possible_choice
     attr_accessor :grid
 
@@ -19,17 +25,17 @@ class Grid
 end
 
 class Game < Grid
-    @@player_one = Array.new
-    @@player_two = Array.new
+    @@player_one = Set.new
+    @@player_two = Set.new
 
     def game
-        puts "**********"
-        puts "This is a tic-tac-toe game for two human players."
-        puts "You're the first player."
+        puts
+        puts "*** This is a tic-tac-toe game for two human players. ***"
+        puts
         loop do
             player_one_turn()
             puts
-                if has_won?
+                if has_won?(@@player_one)
                     puts "The game has ended. Player One has won!"
                     puts
                     return
@@ -37,7 +43,7 @@ class Game < Grid
             break if @@player_one.length == 5
             player_two_turn()
             puts
-                if has_won?
+                if has_won?(@@player_two)
                     puts "The game has ended. Player Two has won!"
                     puts
                     return
@@ -61,9 +67,8 @@ class Game < Grid
         p @@player_two
     end
 
-    def has_won?
-        WINNING_COMBOS.include?(@@player_one.sort) ||
-        WINNING_COMBOS.include?(@@player_two.sort)
+    def has_won?(player)
+        WINNING_COMBOS.any? { |combo| combo <= player}
     end
 end
 
