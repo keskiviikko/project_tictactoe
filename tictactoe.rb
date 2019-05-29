8131dcbb8a4c2aad5a6e93cab9f41417b1fa06a0
Rasmus Rajasalo <rarasmus@outlook.com>
2019-05-29 19:18:26 +0900

    Add two methods, add_x and add_o

--- tictactoe.rb
+++ tictactoe.rb
@@ -7,12 +7,14 @@ class Grid
     ]
     attr_accessor :possible_choice
     attr_accessor :marker_positions
-    attr_accessor :grid
 
     def initialize
         @possible_choice = [1,2,3,4,5,6,7,8,9]
         @marker_positions = [1,2,3,4,5,6,7,8,9]
-        @grid = "
+    end
+    
+    def grid
+        puts "
                 |----|----|----|   
                 |  #{@marker_positions[0]} |  #{@marker_positions[1]} |  #{@marker_positions[2]} |
                 |----|----|----|
@@ -22,11 +24,6 @@ class Grid
                 |----|----|----|
                 "
     end
-    
-    def add_markers
-        
-        puts @grid
-    end
 end
 
 class Game < Grid
@@ -35,31 +32,38 @@ class Game < Grid
 
     def game
         puts
-        puts "*** This is a tic-tac-toe game for two human players. ***"
+        puts "  This is a tic-tac-toe game for two human players."
+        puts "  Be careful to select available numbers only!"
         puts
         loop do
             player_one_turn()
             puts
                 if has_won?(@@player_one)
-                    puts "The game has ended. Player One has won!"
+                    puts "  The game has ended. Player One has won!"
                     puts
+                    add_x
+                    grid
                     return
                 end
             break if @@player_one.length == 5 || @@player_one.include?(nil)
             player_two_turn()
             puts
                 if has_won?(@@player_two)
-                    puts "The game has ended. Player Two has won!"
+                    puts "  The game has ended. Player Two has won!"
                     puts
+                    add_o
+                    grid
                     return
                 end
+            break if @@player_two.include?(nil)
         end
     end
 
     def player_one_turn()
         puts "Player One, make your choice:"
         p @possible_choice
-        add_markers
+        add_o
+        grid
         @@player_one << @possible_choice.delete(gets.chomp.to_i)
         p "Player One has chosen: #{@@player_one}"
     end
@@ -67,9 +71,70 @@ class Game < Grid
     def player_two_turn()
         puts "Player Two, make your choice:"
         p @possible_choice
-        add_markers
+        add_x
+        grid
         @@player_two << @possible_choice.delete(gets.chomp.to_i)
         p "Player Two has chosen: #{@@player_two}"
+    end
+
+    def add_x
+        if @@player_one.include?(1)
+            @marker_positions[0] = "X"
+        end
+        if @@player_one.include?(2)
+            @marker_positions[1] = "X"
+        end
+        if @@player_one.include?(3)
+            @marker_positions[2] = "X"
+        end
+        if @@player_one.include?(4)
+            @marker_positions[3] = "X"
+        end
+        if @@player_one.include?(5)
+            @marker_positions[4] = "X"
+        end
+        if @@player_one.include?(6)
+            @marker_positions[5] = "X"
+        end
+        if @@player_one.include?(7)
+            @marker_positions[6] = "X"
+        end
+        if @@player_one.include?(8)
+            @marker_positions[7] = "X"
+        end
+        if @@player_one.include?(9)
+            @marker_positions[8] = "X"
+        end
+    end
+
+    def add_o
+        if @@player_two.include?(1)
+            @marker_positions[0] = "O"
+        end
+        if @@player_two.include?(2)
+            @marker_positions[1] = "O"
+        end
+        if @@player_two.include?(3)
+            @marker_positions[2] = "O"
+        end
+        if @@player_two.include?(4)
+            @marker_positions[3] = "O"
+        end
+        if @@player_two.include?(5)
+            @marker_positions[4] = "O"
+        end
+        if @@player_two.include?(6)
+            @marker_positions[5] = "O"
+        end
+        if @@player_two.include?(7)
+            @marker_positions[6] = "O"
+        end
+        if @@player_two.include?(8)
+            @marker_positions[7] = "O"
+        end
+        if @@player_two.include?(9)
+            @marker_positions[8] = "O"
+        end
     end
 
     def has_won?(player)
